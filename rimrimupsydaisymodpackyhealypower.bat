277d039a0ce0713bf31fa32af0c31e20c544d26e
HBonesEsq <rebeccamaeforbes@gmail.com>
2024-10-03 23:46:55 -0500

    Add files via upload
    A little thing that hopefully works. Run as admin.

---
+++ rimrimupsydaisymodpackyhealypower.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Rimrim Upsy-Daisy Modpacky Healy Power!
+ECHO =======================================
+DEL /p Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\*
+DEL /p Program Files (x86)\Steam\steamapps\workshop\content\294100\*
+DEL /p AppData\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\*
+ECHO ...Did it work?
+PAUSE
